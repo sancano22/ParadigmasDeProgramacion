3ecd0e4d50b46496ee1fab331b3f660fb92d377f
Cliqui-robot <roboticaeducativapucv@gmail.com>
2023-09-12 18:42:53 -0300

    datos usando JSON y POO

--- Python/Datos/inicio.py
+++ Python/Datos/inicio.py
@@ -4,13 +4,16 @@ from datetime import datetime
 format_data="%m/%d/%Y %H:%M:%S"
 
 class Dato:
+    def __init__(self):
+        pass 
+     
     def __init__(self,nombres,apellidos,genero,fechaNacimiento,email,carrera):
         self.fechaNacimiento=datetime.strptime(fechaNacimiento, format_data)
         self.nombres=nombres
         self.apellidos=apellidos
         self.genero=genero
         self.email=email
-        self.carrera=carrera
+        self.carrera=carrera 
     
     def calcularEdad(self):
         self.edad=datetime.now().year - self.fechaNacimiento.year
@@ -19,7 +22,9 @@ class Dato:
 if __name__=="__main__":
     archivo= open('data.json')
     dato=json.load(archivo)
+ 
     for i in range(0,len(dato)):
+        datos=Dato(dato)
         datos=Dato(dato[i]["nombre"],dato[i]["apellidos"],
                    dato[i]["genero"],dato[i]["fechaNacimiento"],dato[i]["email"],dato[i]["carrera"])
-        print(datos.calcularEdad())
+        
